543a74605abdb59fbc3f13145c447df577df9a9a
abhishekk-web <abhi.chatterjee38@gmail.com>
2023-02-05 14:30:26 +0530

    Messaging is working perfectly in chat application

--- router/message.js
+++ router/message.js
@@ -3,40 +3,52 @@ const fs = require('fs');
 
 const router = express.Router();
 
+router.get('/', (req, res)=> {
 
+    fs.readFile("message.txt", (err,data)=> {
 
-    router.get('/', (req, res, next)=>{
-       
-        fs.readFile('messages.txt', (err, data)=>{
-
-            if(err){
-
-                console.log(err);
-                data = 'There is no data'
-
-            }
-            res.send(`${data}<form action="/" onsubmit="document.getElementById('name').value=localStorage.getItem('name')"  method="POST" ><input type="text" name="message" id="message" placeholder="write a message"><input name="name" id="name"><br><br><button type="submit">Send</button></form>`) 
-
-        })
-
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(
+                `${data}<form action="/message"  onsubmit='document.getElementById("name").value = localStorage.getItem("name")' method="POST" )><label>Enter the message</label><input type='text' name='message' id='messsage'><input type="hidden" id='name' name='name'><button type='submit'>Send</button></form>`
+            )
+        }
         
+
     })
+
     
-    
 
 
-    router.post('/', (req, res, next)=>{
+})
 
-        console.log(`${req.body.name}:${req.body.message}`);
-        fs.writeFile("messages.txt", `${req.body.name}: ${req.body.message}`, {flag: 'a'},(err)=>{
-            if(err){
-                console.log(err);
-            }
-            else{
+router.post("/message", (req, res)=> {
+
+    console.log(req.body);
+    const username = req.body.name;
+    console.log("the username is"+username);
+    const message = req.body.message;
+    console.log(message);
+    fs.writeFile("message.txt",`${req.body.name}: ${req.body.message}`, {flag: 'a'}, (err) => {
+        if(err){
+            console.log(err);
+        }
+        else{
             res.redirect('/');
-            }
-        })
+        }
+    } )
 
-    })
 
-    module.exports = router;
+   
+
+})
+
+
+
+module.exports = router;
+
+
+
+
